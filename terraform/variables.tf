c47e4178f7f460d59247725b5db60f7ff013fbcd
Noel Mathews <noelmathews123@gmail.com>
2026-07-28 17:14:45 +0530

    chore:terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,24 @@
 variable "aws_region" {
-    description = "aws region"
-    type = string
-    default = "ap-south-1"
+  description = "aws region"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "availability_zone" {
-    description = "aws availability zone"
-    type = string
-    default = "ap-south-1a"
+  description = "aws availability zone"
+  type        = string
+  default     = "ap-south-1a"
 }
 
 variable "project_name" {
-    description = "Project Name"
-    type = string
-    default = "cuia"
+  description = "Project Name"
+  type        = string
+  default     = "cuia"
 }
 
 variable "instance_type" {
-    description = "EC2 type"
-    type = string
-    default = "t3.small"
+  description = "EC2 type"
+  type        = string
+  default     = "t3.small"
 }
 
