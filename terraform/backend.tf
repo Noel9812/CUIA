853564ba9dc9f8d1c36198f13ad9cef81503c40a
Noel Mathews <noelmathews123@gmail.com>
2026-08-17 11:54:43 +0530

    updated terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "poc-tf-state-087607092654-ap-south-1-an"
+    bucket = "poc-tf-state-163081953997-ap-south-1-an"
     key    = "cuia/terraform.tfstate"
     region = "ap-south-1"
   }
